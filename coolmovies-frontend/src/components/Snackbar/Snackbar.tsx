2c5d76b7d9881b981e32a7005ff83f345ba63fde
Filipe <burgodev@gmail.com>
2022-07-03 20:22:42 -0300

    Snackbar Component

---
+++ coolmovies-frontend/src/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Snackbar as MuiSnackbar } from '@mui/material';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import { ISnackbar } from '../../types/typings';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
+  function Alert(props, ref) {
+    return (
+      <MuiAlert
+        elevation={6}
+        ref={ref}
+        variant="filled"
+        {...props}
+      />
+    );
+  }
+);
+
+const Snackbar = ({
+  open,
+  severity,
+  message,
+  onClose,
+}: ISnackbar) => {
+  return (
+    <MuiSnackbar
+      open={open}
+      autoHideDuration={6000}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      onClose={onClose}>
+      <Alert
+        onClose={onClose}
+        severity={severity}
+        sx={{ width: '100%' }}>
+        {message}
+      </Alert>
+    </MuiSnackbar>
+  );
+};
+
+export default Snackbar;
